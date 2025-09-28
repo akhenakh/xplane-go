cbded6ad75f00f209604cf2528c078e3629ef6a6
Fabrice Aneche <akh@inair.space>
2025-09-28 19:26:14 -0400

    also debug to stdout

--- util/util.go
+++ util/util.go
@@ -4,13 +4,18 @@ package util
 // #include <stdlib.h>
 // #include "XPLMUtilities.h"
 import "C"
-import "unsafe"
+import (
+	"fmt"
+	"unsafe"
+)
 
 // DebugString writes a string to the X-Plane Log.txt file.
 func DebugString(s string) {
 	cs := C.CString(s)
 	defer C.free(unsafe.Pointer(cs)) // Now C.free is recognized
 	C.XPLMDebugString(cs)
+	// Also print the message on stdout
+	fmt.Println(s)
 }
 
 // GetSystemPath returns the full path to the X-Plane installation directory.
